191ab5f3c275981a71178c76e4cbfe596109d108
xavier-meyer <xavier.01@orange.fr>
2022-09-13 15:15:03 +0200

    feat: fichiers view

---
+++ View/front/Carte.php
@@ -0,0 +1,16 @@
+<div class="col-3">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">
+                <?= $auteur->name ?>
+            </h5>
+            <p class="card-text">
+                Citation de l'auteur : <?= $auteur->getCitationByAuteur(); ?>
+            </p>
+        </div>
+    </div>
+</div>
+
+
+
+
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-
+require_once 'View/front/partial/header.php';
 require_once 'App/Service/Database.php';
 require_once 'App/Repository/AuteursRepository.php';
 require_once 'App/Repository/CitationRepository.php';
+
 
 use App\Repository\CitationRepository;
 use App\Repository\AuteursRepository;
@@ -17,3 +18,7 @@ var_dump($auteur->findAll());
 
 $citations = new CitationRepository();
 var_dump($citations->findAll());
+
+
+
+require_once 'View/front/partial/footer.php';
